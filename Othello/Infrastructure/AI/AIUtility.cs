0ea0d5580c3de42a01c6ea9660908e8342fa9ffb
huurou0721 <0ay386063pt107k@ezweb.ne.jp>
2020-04-13 22:37:56 +0900

    AIUtility追加

---
+++ Othello/Infrastructure/AI/AIUtility.cs
@@ -0,0 +1,19 @@
+﻿using Othello.Domain.Model;
+using System;
+
+namespace Othello.Infrastructure.AI
+{
+    public static class AIUtility
+    {
+        private static Random Random { get; } = new Random();
+
+        public static Position DecidePositionRandom(Teban teban, IBoard board)
+        {
+            var legalList = board.GetLegalPosistionList(teban);
+            return legalList.Count != 0
+                ? legalList[Random.Next(legalList.Count)]
+                : throw (new ArgumentException("合法手が存在しません", nameof(legalList)));
+        }
+    }
+}
+}
