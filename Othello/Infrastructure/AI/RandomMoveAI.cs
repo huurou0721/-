4618e6fbef77cf0cfa77f2810d4b87fbdcb7929d
huurou0721 <57633912+huurou0721@users.noreply.github.com>
2020-04-13 22:41:18 +0900

    AIUtility追加 (#1)

--- Othello/Infrastructure/AI/RandomMoveAI.cs
+++ Othello/Infrastructure/AI/RandomMoveAI.cs
@@ -5,14 +5,9 @@ namespace Othello.Infrastructure.AI
 {
     public class RandomMoveAI : IAI
     {
-        private Random Random { get; } = new Random();
-
         public Position DecideMove(Teban teban, IBoard board)
         {
-            var legalList = board.GetLegalPosistionList(teban);
-            return legalList.Count != 0 ?
-                legalList[Random.Next(legalList.Count)] :
-                throw (new ArgumentException("合法手が存在しません", nameof(legalList)));
+            return AIUtility.DecidePositionRandom(teban, board);
         }
     }
 }
